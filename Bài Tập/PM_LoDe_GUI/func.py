e8811d2c3dce993af1690c88086bdf8be6320d8f
Phùng Bách <75926028+phungbach@users.noreply.github.com>
2023-01-11 18:23:57 +0700

    new file:   "B\303\240i T\341\272\255p/PM_LoDe_GUI/func.py" 	deleted:    Qly_hocvien_GUI/__pycache__/connect_DB.cpython-310.pyc 	deleted:    Qly_hocvien_GUI/__pycache__/func.cpython-310.pyc 	deleted:    Qly_hocvien_GUI/connect_DB.py 	deleted:    Qly_hocvien_GUI/main_GUI.py 	deleted:    Quanlynhansu/__pycache__/connect_DB.cpython-310.pyc 	deleted:    Quanlynhansu/connect_DB.py 	deleted:    Quanlynhansu/func.py 	deleted:    Quanlynhansu/main_GUI.py 	"B\303\240i T\341\272\255p/PM_LoDe_GUI/main.py" 	"B\303\240i T\341\272\255p/Qly_hocvien_GUI/" 	"B\303\240i T\341\272\255p/Quanlynhansu_GUI/" 	gui.py

---
+++ Bài Tập/PM_LoDe_GUI/func.py
@@ -0,0 +1,33 @@
+import random
+from tkinter.scrolledtext import ScrolledText
+import main
+
+giaidacbiet = random.randint(1,99999)
+giai1 = random.randint(1,99999)
+giai2a = random.randint(1,99999)
+giai2b = random.randint(1,99999)
+giai3a = random.randint(1,99999)
+giai3b = random.randint(1,99999)
+giai3c = random.randint(1,99999)
+giai4a = random.randint(1,9999)
+giai4b = random.randint(1,9999)
+giai4c = random.randint(1,9999)
+giai4d = random.randint(1,9999)
+giai5a = random.randint(1,9999)
+giai5b = random.randint(1,9999)
+giai5c = random.randint(1,9999)
+giai5d = random.randint(1,9999)
+giai5e = random.randint(1,9999)
+giai5f = random.randint(1,9999)
+giai6a = random.randint(1,999)
+giai6b = random.randint(1,999)
+giai6c = random.randint(1,999)
+giai7a = random.randint(1,99)
+giai7b = random.randint(1,99)
+giai7c = random.randint(1,99)
+
+def show_data():
+    
+    pass
+
+print(giaidacbiet)
